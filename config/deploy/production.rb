d743aa14e1b5e6da43a3fd470b5bb7209b195e05
willy <willypoteloin@yahoo.fr>
2016-02-09 22:49:23 +0100

    capistrano config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'w-creation.fr', user: 'willy', roles: %w{web}
+server 'w-creation.fr', user: 'willy', roles: %w{web}, port: 650
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
