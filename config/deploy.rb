76213795109619ac6c0360d27005bd3cd7d81cd2
willy <willypoteloin@yahoo.fr>
2016-02-09 22:37:15 +0100

    capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'capi'
+set :repo_url, 'git@gitlab.com:WillyPoteloin/Portfolio.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/volume1/web/my_app_name'
 
 # Default value for :scm is :git
 # set :scm, :git
